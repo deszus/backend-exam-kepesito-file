b30672f85966a75ba49ca94f44bbecb00438b859
deszus <68805470+deszus@users.noreply.github.com>
2021-04-29 11:41:02 +0200

    1st upload

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,22 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            FileWriter fileOut = new FileWriter(output);
+            for (Map.Entry<String, String> phoneEntry : contacts.entrySet()) {
+                fileOut.write(phoneEntry.getKey() + ": " + phoneEntry.getValue() + System.lineSeparator());
+            }
+            fileOut.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
